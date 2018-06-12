5fd347ecdcbc149d07e2456668b35e2a963d4a6b
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-06-12 14:28:59 +0200

    Don't format balances until "aepp-sdk" returns them as Numbers

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,13 +1,10 @@
-const blockies = require('ethereum-blockies-png')
-const numeral = require('numeral')
-
 export default {
   methods: {
     forwardEvent (event) {
       this.$emit(event.type, event)
     },
     readableToken (balance) {
-      return numeral(balance / 10e12).format('0,0.[000]')
+      return balance
     }
   }
 }
