7adbda980ad127bb14695154ca1b6a675cc47c01
Denis Davidyuk <Denis_Davidyuk@hotmail.com>
2018-09-24 06:35:17 +0200

    Fix indents and line breaks

--- src/components/ae-check/ae-check.vue
+++ src/components/ae-check/ae-check.vue
@@ -14,6 +14,7 @@
     </span>
   </label>
 </template>
+
 <script>
 export default {
   name: 'ae-check',
@@ -26,19 +27,15 @@ export default {
      * ID of the component/input
      */
     id: String,
-
     /**
      * Name of component
      */
     name: String,
-
     /**
      * value of component
      */
     value: { type: [String, Number, Boolean], default: undefined },
-
     checked: { type: [Array, String, Number, Boolean], default: false },
-
     /**
      * Define the type of the input
      */
@@ -46,7 +43,6 @@ export default {
       type: String,
       default: 'checkbox',
     },
-
     /**
      * Puts the component in disabled state
      */
@@ -77,46 +73,47 @@ export default {
   },
 };
 </script>
+
 <style lang="scss" scoped>
-  @import '../../styles/variables/colors';
-  @import '../../styles/variables/animations';
-  @import '../../styles/variables/typography';
+@import '../../styles/variables/colors';
+@import '../../styles/variables/animations';
+@import '../../styles/variables/typography';
 
-  .ae-check {
-    input {
-      display: none;
+.ae-check {
+  input {
+    display: none;
 
-      &:checked + .indicator {
-        background: $color-emphasis;
-        border-color: $color-emphasis;
-        color: $color-white;
-      }
-
-      &:disabled + .indicator {
-        cursor: not-allowed;
-        background: $color-neutral-positive-1;
-        border-color: $color-neutral-positive-2;
-      }
+    &:checked + .indicator {
+      background: $color-emphasis;
+      border-color: $color-emphasis;
+      color: $color-white;
     }
 
-    .indicator {
-      box-sizing: content-box;
-      width: rem(22px);
-      height: rem(22px);
-      display: inline-block;
-      cursor: pointer;
-      transition-duration: $base-transition-time;
-      transition-property: background-color, border-color, color;
-      background: $color-white;
-      border: 2px solid $color-neutral-positive-1;
-      border-radius: 50%;
-      box-shadow: 0 0 16px $color-shadow-alpha-15;
-      color: transparent;
-      font-family: $font-sans;
-      font-size: rem(16px);
-      font-weight: 700;
-      line-height: rem(22px);
-      text-align: center;
+    &:disabled + .indicator {
+      cursor: not-allowed;
+      background: $color-neutral-positive-1;
+      border-color: $color-neutral-positive-2;
     }
   }
+
+  .indicator {
+    box-sizing: content-box;
+    width: rem(22px);
+    height: rem(22px);
+    display: inline-block;
+    cursor: pointer;
+    transition-duration: $base-transition-time;
+    transition-property: background-color, border-color, color;
+    background: $color-white;
+    border: 2px solid $color-neutral-positive-1;
+    border-radius: 50%;
+    box-shadow: 0 0 16px $color-shadow-alpha-15;
+    color: transparent;
+    font-family: $font-sans;
+    font-size: rem(16px);
+    font-weight: 700;
+    line-height: rem(22px);
+    text-align: center;
+  }
+}
 </style>
